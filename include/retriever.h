a03980507960fe24aece56447685fecb89a89027
aymanervn <aymanereventon@gmail.com>
2026-07-21 00:32:27 +0100

    feat: add retriever.h as spec

--- include/retriever.h
+++ include/retriever.h
@@ -91,7 +91,7 @@
  *   <- .
  */
 
-#define RTV_VERSION "0.2.0"
+#define RTV_VERSION "0.9.5"
 #define RTV_PIPE_NAME "\\\\.\\pipe\\retriever"
 #define RTV_SOCK_PATH "/tmp/retriever.sock"
 #define RTV_MAX_LINE 4128
